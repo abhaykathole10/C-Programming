0417ccb7c05f6ba9051a34f9007defbe1eac3815
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-02 15:59:35 +0530

    Checking prime number code in C

---
+++ C-Tasks/prime_number.c
@@ -0,0 +1,36 @@
+//To check if a number is prime or not
+#include<stdio.h>
+
+int main(){
+
+        while(1)
+        {
+                int num,i;
+                printf("Enter number: ");
+                scanf("%d",&num);
+                if(i==1)
+                    printf("Neither Prime nor Unprime");
+                i = 2;
+                while(i <= num-1)
+                {
+                    if(num % i == 0)
+                    {
+                        printf("Not Prime\n");
+                        break;
+                    }
+                    i++;
+                }
+
+                if( i == num)
+                {
+                    printf("Prime\n");
+            }
+
+        }
+
+
+
+
+}
+
+
