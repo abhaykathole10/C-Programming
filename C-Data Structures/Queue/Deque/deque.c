0e4b544110f09a6007790aab524fa7e2184ef93d
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-04 18:20:54 +0530

    Deque data Structure

---
+++ C-Data Structures/Queue/Deque/deque.c
@@ -0,0 +1,8 @@
+/*Queue in which deletion and insertion is possible from both ends
+Generalized form of queue
+There are two types of Deque:
+1.Input Restricted(insertion at only one end) 2.Output Restricted(deletion only at one end)
+Applications of deque:
+1. Deque can be used as both stack and queue, as it supports both operations.
+2. Deque can be used as a palindrome checker means that if we read the string
+ from both ends, the string would be the same.*/
