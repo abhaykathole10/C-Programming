da950fa2956343c6b91fb374216f03c1b9498423
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-23 23:22:21 +0530

    Inserting an element at a location in array

--- C-Data Structures/Array/element_insertion.c
+++ C-Data Structures/Array/element_insertion.c
@@ -2,34 +2,50 @@
 #include<stdio.h>
 int main(){
     int a[50];
+
     int i,n;
     int new,loc;
+
     printf("Enter size: ");
     scanf("%d",&n);
+
     printf("Enter %d elements\n",n);
 
+    //Writing Array elements
     for(i=0 ; i < n ; i++)
         {
             printf("Enter number: ");
             scanf("%d",&a[i]);
         }
+
+    //Reading array elements
     for(i=0 ; i < n ; i++)
         {
             printf("%d\n",a[i]);
         }
+
+    //Reading the 'number'
     printf("Enter number you want to insert: ");
     scanf("%d",&new);
+
+    //Reading the location
     printf("Enter location: ");
     scanf("%d",&loc);
 
-    for (i=n-1 ; i > loc; i--)
+    //Traversing till the loc and at the same time shifting the elements by one
+    for (i = n-1 ; i > loc; i--)
         {
             a[i+1] = a[i];
         }
+
+    //Increasing the size of array by One
     n = n+1;
+
+    //Assigning 'new' element to the "loc"
     a[loc] = new;
 
-    for(i=0 ; i < n ; i++)
+    //Printing Array
+    for(i = 0 ; i < n ; i++)
     {
         printf("%d\n",a[i]);
     }
