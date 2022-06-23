9a7962e407765cc2f7563a6e38a680a1b3ab46b2
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-23 22:57:54 +0530

    Calling a function in a function

--- C-Basics/calling_functions.c
+++ C-Basics/calling_functions.c
@@ -1,19 +1,33 @@
 #include<stdio.h>
 
+//Function prototype
 void argentina();
 void brazil();
 
+//main() function
 int main(){
     printf("I am home\n");
+
+    //Calling function 1
     argentina();
+
     printf("Back home\n");
+
     return 0;
 }
+
+//Function 1
 void argentina(){
+
     printf("I am in Argentina\n");
+
+    //Calling function 2
     brazil();
 }
+
+//Function 2
 void brazil(){
+
     printf("I am in brazil\n");
 }
 
