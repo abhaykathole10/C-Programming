f73aec35784fb1990a1ed82ae76ffbee7a9ea246
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-23 22:59:14 +0530

    Switch Case method

--- C-Basics/switch-case.c
+++ C-Basics/switch-case.c
@@ -2,21 +2,28 @@
 
 int main(){
 
+    //Declaring literal ex. int ch
     char ch;
+
     printf("Enter a chracter: ");
     scanf("%c",&ch);
 
     switch(ch) {
+        //"Case insensitive" i.e 'a' and 'A' are same
         case 'a':
         case 'A':
             printf("You pressed a\n");
             break;
+
         case 'b':
             printf("You pressed b\n");
             break;
+
         case 'c':
             printf("You pressed c\n");
             break;
+
+        //Default method is optional, if not present program will just return
         default:
             printf("Invalid input");
     }
