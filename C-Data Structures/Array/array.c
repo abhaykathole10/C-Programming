3b496d92c9a27657ff373525d5f1e66d7685d2f7
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-23 23:21:10 +0530

    Declaring, Writing and Reading an Array

--- C-Data Structures/Array/array.c
+++ C-Data Structures/Array/array.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main(){
+
+    //Declaring an array of size 10
     int a[10];
+
     int i,n;
 
     printf("Enter number of elements you want: ");
     scanf("%d",&n);
 
+    //Writing
     for(i=0 ; i < n ; i++)
         {
             printf("Enter element: ");
             scanf("%d",&a[i]);
         }
+
+    //Reading
     for(i=0 ; i < n ; i++)
         {
             printf("%d\t",a[i]);
