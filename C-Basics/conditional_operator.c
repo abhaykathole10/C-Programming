ed33ebeed71edff8e91cd732530019821ae11c47
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-23 22:58:26 +0530

    Conditional / Ternary Operator

--- C-Basics/conditional_operator.c
+++ C-Basics/conditional_operator.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main(){
+
     int i;
     printf("Enter i: ");
     scanf("%d",&i);
+
+    //"?" and ":" are called the conditional operators
+    //This is also a ternary function format
+
     (i==1 ? printf("Abhay"):printf("Emily"));
+
     return 0;
 }
